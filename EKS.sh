4e774f64f58c7b799fcbc5b54c036024f1e77d01
tushar2809 <66154356+alt-tushar2809@users.noreply.github.com>
2021-03-16 19:32:17 +0530

    Create EKS.sh
    initial commit

---
+++ EKS.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+aws-iam-authenticator help
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+kubectl version --short --client
+aws eks --region ap-south-1 describe-cluster --name  eks-demo --query cluster.status
+aws eks --region ap-south-1 update-kubeconfig --name eks-demo
+kubectl get svc
