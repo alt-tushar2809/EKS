c1dbf620d28069ad1fae2b813c41e372a84458f8
tushar2809 <66154356+alt-tushar2809@users.noreply.github.com>
2021-03-27 18:27:37 +0530

    Update EKS.sh
    Second Commit

--- EKS.sh
+++ EKS.sh
@@ -1,4 +1,13 @@
-#/bin/bash
+ #/bin/bash
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+yum install unzip -y
+unzip awscli-bundle.zip
+./awscli-bundle/install -b ~/bin/aws
+echo $PATH | grep ~/bin      
+export PATH=~/bin:$PATH   
+export PATH=~/.local/bin:$PATH
+source ~/.bash_profile
+aws --version
 curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
 mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
